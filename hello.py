9c08b40b31e3d46aa53004d6323de946dfd54ef8
Tom Boyce <tboyce12@gmail.com>
2025-05-02 19:03:08 -0700

    Add create and read CRUD operations

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort, request
 
 app = Flask(__name__)
 tasks = [
@@ -6,9 +6,24 @@ tasks = [
     {"pk": 1, "title": "Task 1"},
     {"pk": 2, "title": "Task 2"},
 ]
-task_pk = len(tasks)
+tasks_pk = len(tasks)
 
-@app.route("/")
-@app.route("/tasks/")
-def task_list():
+@app.get("/")
+@app.get("/tasks/")
+def list_tasks():
     return tasks
+
+@app.get("/tasks/<int:pk>")
+def get_task(pk):
+    t = next((t for t in tasks if t["pk"] == pk), None)
+    if not t:
+        abort(404)
+    return t
+
+@app.post("/tasks")
+def create_task():
+    title = request.form["title"]
+
+    global tasks, tasks_pk
+    tasks.append({"pk": tasks_pk, "title": title})
+    return {"ok": True}
