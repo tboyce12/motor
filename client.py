a71234ec44d995c5b70238884327a5879eb88b89
Tom Boyce <tboyce12@gmail.com>
2025-05-09 17:43:47 -0700

    Use PyJWT to authenticate requests

--- client.py
+++ client.py
@@ -2,29 +2,53 @@ import json
 import requests
 import sys
 
-def get(url):
-    response = requests.get(url)
+def get(url, token=None):
+    if token:
+        response = requests.get(
+            url,
+            headers={"Authorization": f"Bearer {token}"}
+        )
+    else:
+        response = requests.get(url)
     if not response.ok:
         print(f"Error HTTP status {response.status_code}", file=sys.stderr)
         return
     print(json.dumps(response.json(), indent=4))
 
-def post(url, data):
-    response = requests.post(url, json=data)
+def post(url, data, token=None):
+    if token:
+        response = requests.post(
+            url, json=data,
+            headers={"Authorization": f"Bearer {token}"}
+        )
+    else:
+        response = requests.post(url, json=data)
     if not response.ok:
         print(f"Error HTTP status {response.status_code}", file=sys.stderr)
         return
     print(json.dumps(response.json(), indent=4))
 
-def put(url, data):
-    response = requests.put(url, json=data)
+def put(url, data, token=None):
+    if token:
+        response = requests.put(
+            url, json=data,
+            headers={"Authorization": f"Bearer {token}"}
+        )
+    else:
+        response = requests.put(url, json=data)
     if not response.ok:
         print(f"Error HTTP status {response.status_code}", file=sys.stderr)
         return
     print(json.dumps(response.json(), indent=4))
 
-def delete(url):
-    response = requests.delete(url)
+def delete(url, token=None):
+    if token:
+        response = requests.delete(
+            url,
+            headers={"Authorization": f"Bearer {token}"}
+        )
+    else:
+        response = requests.delete(url)
     if not response.ok:
         print(f"Error HTTP status {response.status_code}", file=sys.stderr)
         return
--- server.py
+++ server.py
@@ -1,23 +1,51 @@
-from flask import Flask, abort, request
+import jwt
+from flask import Flask, abort, request, g
+from functools import wraps
 import db
 
+SECRET_KEY = "change me!"
 app = Flask(__name__)
-
 db.init()
+
+def require_login(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        header = request.headers.get("Authorization")
+        if not header:
+            abort(401)
+        token = header.split()[1]
+        try:
+            g.user_id = jwt.decode(
+                token, SECRET_KEY, algorithms=["HS256"]
+            )["user_id"]
+        except:
+            abort(401)
+        return f(*args, **kwargs)
+    return wrapper
+
+@app.post("/login")
+def user_login():
+    token = jwt.encode({"user_id": "foo"}, SECRET_KEY)
+    return {"token": token}
 
 @app.get("/")
 @app.get("/tasks/")
-def list_tasks():
+@require_login
+def task_list():
     return [{"pk": t[0], "title": t[1], "status": t[2]}
             for t in db.list_tasks()]
 
 @app.get("/tasks/<int:pk>")
-def get_task(pk):
+@require_login
+def task_get(pk):
     t = db.get_task(pk)
+    if not t:
+        abort(404)
     return {"pk": t[0], "title": t[1], "status": t[2]}
 
 @app.post("/tasks")
-def create_task():
+@require_login
+def task_create():
     data = request.json
     if not data:
         abort(400)
@@ -25,10 +53,11 @@ def create_task():
     if not title:
         abort(400)
     db.create_task(title)
-    return {"ok": True}
+    return {}
 
 @app.put("/tasks/<int:pk>")
-def update_task(pk):
+@require_login
+def task_update(pk):
     data = request.json
     if not data:
         abort(400)
@@ -37,9 +66,10 @@ def update_task(pk):
     if not title or not status:
         abort(400)
     db.update_task(pk, title, status)
-    return {"ok": True}
+    return {}
 
 @app.delete("/tasks/<int:pk>")
-def delete_task(pk):
+@require_login
+def task_delete(pk):
     db.delete_task(pk)
-    return {"ok": True}
+    return {}
